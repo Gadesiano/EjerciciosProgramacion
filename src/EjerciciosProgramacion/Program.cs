94d1b51dbde90f523e158b04e56fcc93a4922b12
Gadesiano <titto.rosales@gmail.com>
2026-05-12 18:12:53 +0200

    feat: add IEjercicio interface and exercise runner menu

--- src/EjerciciosProgramacion/Program.cs
+++ src/EjerciciosProgramacion/Program.cs
@@ -1,2 +1,32 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EjerciciosProgramacion;
+using System.Runtime.Serialization.Formatters;
+
+var ejercicios = new List<IEjercicio>();
+{
+
+}
+
+if (ejercicios.Count == 0)
+{
+    Console.WriteLine("No hay ejercicios disponibles TODAVÍA...");
+    return;
+}
+
+Console.WriteLine("=== EJERCICIOS DE PROGRAMACIÓN ===\n");
+
+for (int i = 0; i < ejercicios.Count; i++)
+{
+    Console.WriteLine($"{i + 1}.- {ejercicios[i].Titulo}");
+}
+
+Console.Write("\nSelecciona un ejercicio: ");
+string? input = Console.ReadLine();
+
+if (!int .TryParse(input, out int seleccion) || seleccion < 1 || seleccion > ejercicios.Count)
+{
+    Console.WriteLine("Opción no válida.");
+    return;
+}
+
+Console.WriteLine($"\n--- {ejercicios[seleccion-1].Titulo} ---\n");
+ejercicios[seleccion-1].Run();
